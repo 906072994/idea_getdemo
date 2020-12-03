45489c3a6ad76c40d689f9c110c54e63305da8f4
lin <906072994@qq.com>
2020-12-03 15:23:04 +0800

    小丽添加11111

--- src/main/java/cn/itcast/entity/User.java
+++ src/main/java/cn/itcast/entity/User.java
@@ -1,12 +1,12 @@
 package cn.itcast.entity;
 
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 public class User {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git 2");
-        System.out.println("hello git hello world");    
+        System.out.println("hello git hello world");
+        System.out.println("111111");
     }
 }
