6132503ceedbea4c655956a214b74bfe0d34cada
林光标 <906072994@qq.com>
2020-12-03 14:16:00 +0800

    添加一行

--- src/main/java/cn/itcast/entity/User.java
+++ src/main/java/cn/itcast/entity/User.java
@@ -7,5 +7,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git 2");
+        System.out.println("hello git hello world");    
     }
 }
