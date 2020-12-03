7aa30bb5af8c98e46b8e2b68c955efc31a0e6240
lin <906072994@qq.com>
2020-12-03 14:13:38 +0800

    hello git 2

--- src/main/java/cn/itcast/entity/User.java
+++ src/main/java/cn/itcast/entity/User.java
@@ -6,5 +6,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class User {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git 2");
     }
 }
